ddb5306a3b9f3630ae2969681baf52cab7ed68f5
soohyunc <soohyunc@e14362a3-d196-dc11-b158-00301b3f055e>
2009-02-08 22:37:15 +0000

    re-structure directory

--- tfwc/exercise/comp_05.1/rmarchives.sh
+++
@@ -1,7 +0,0 @@
-# $Id$
-
-# delete all trace files
-for i in $( ls $PWD/archives/ ); do
-	echo "deleting: $i"
-	rm -f $PWD/archives/$i
-done
--- tfwc/exercise/comp_05.2/rmgraph.sh
+++
@@ -1,7 +0,0 @@
-# $Id$
-
-# delete all graphs
-for i in $( ls $PWD/graph/ ); do
-	echo "deleting: $i"
-	rm -f $PWD/graph/$i
-done
