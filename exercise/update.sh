d08be85ee205ff9ff2ea21275b62c73ca728b497
soohyunc <soohyunc@e14362a3-d196-dc11-b158-00301b3f055e>
2009-02-19 19:48:49 +0000

    Get working for updating exercise directory script.

--- exercise/update.sh
+++ exercise/update.sh
@@ -1,11 +1,26 @@
 # Update TFWC/exercise 
 # $Id$
 
-for target in ibex echidna narwhal dugong uakari quokka arkell wombat redkite planet saleem-01-b saleem-02-a saleem-03-a saleem-04-a saleem-05-a saleem-06-a saleem-07-a
+for target in ibex echidna quokka wombat redkite hopback \
+		saleem1 saleem2 saleem3 saleem4 saleem5 hen
 do
-
-echo "entering... $target"
-ssh $target "cd TFWC/exercise && cvs update -d"
-echo "leaving... $target"
-
+	echo "entering... $target"
+	ssh $target "cd TFWC/exercise && svn update"
+	echo "leaving... $target"
 done
+
+# NSF mounted systems
+for target in turkey
+do
+	echo "entering... $target"
+	ssh $target "cd /mnt/disk/soohyunc/TFWC/exercise && svn update"
+	echo "leaving... $target"
+done
+
+for target in cockerel
+do
+	echo "entering... $target"
+	ssh $target "cd /mnt/disk1/soohyunc/TFWC/exercise && svn update"
+	echo "leaving... $target"
+done
+
