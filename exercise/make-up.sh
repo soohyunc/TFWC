44ad9b891e90ecf1fd58e185b87e5a45901b0264
soohyunc <soohyunc@e14362a3-d196-dc11-b158-00301b3f055e>
2009-02-20 01:29:27 +0000

    One SSH session per machine for make-up.sh script.

--- exercise/make-up.sh
+++ exercise/make-up.sh
@@ -5,8 +5,7 @@ for target in ibex echidna quokka wombat redkite hopback \
 		saleem1 saleem2 saleem3 saleem4 saleem5 hen
 do
 	echo "entering... $target"
-	ssh $target "cd TFWC/exercise/comp_05.1/add-on/ && make"
-	ssh $target "cd TFWC/exercise/comp_05.2/add-on/ && make"
+	ssh $target "cd TFWC/exercise/comp_05.1/add-on/ && make && cd && cd TFWC/exercise/comp_05.2/add-on/ && make"
 	echo "leaving... $target"
 	echo ""
 done
@@ -15,8 +14,7 @@ done
 for target in turkey
 do
 	echo "entering... $target"
-	ssh $target "cd /mnt/disk/soohyunc/TFWC/exercise/comp_05.1/add-on/ && make"
-	ssh $target "cd /mnt/disk/soohyunc/TFWC/exercise/comp_05.2/add-on/ && make"
+	ssh $target "cd /mnt/disk/soohyunc/TFWC/exercise/comp_05.1/add-on/ && make && cd && cd /mnt/disk/soohyunc/TFWC/exercise/comp_05.2/add-on/ && make"
 	echo "leaving... $target"
 	echo ""
 done
@@ -24,8 +22,7 @@ done
 for target in cockerel
 do
 	echo "entering... $target"
-	ssh $target "cd /mnt/disk1/soohyunc/TFWC/exercise/comp_05.1/add-on/ && make"
-	ssh $target "cd /mnt/disk1/soohyunc/TFWC/exercise/comp_05.2/add-on/ && make"
+	ssh $target "cd /mnt/disk1/soohyunc/TFWC/exercise/comp_05.1/add-on/ && make && cd && cd /mnt/disk1/soohyunc/TFWC/exercise/comp_05.2/add-on/ && make"
 	echo "leaving... $target"
 	echo ""
 done
