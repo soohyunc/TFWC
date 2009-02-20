db63aebc3d81a8b81db49d5a6ff13ba0017a45c2
soohyunc <soohyunc@e14362a3-d196-dc11-b158-00301b3f055e>
2009-02-20 01:19:03 +0000

    Added "make" update for comp_05.x/add-on

---
+++ exercise/make-up.sh
@@ -0,0 +1,32 @@
+# Update TFWC/exercise 
+# $Id$
+
+for target in ibex echidna quokka wombat redkite hopback \
+		saleem1 saleem2 saleem3 saleem4 saleem5 hen
+do
+	echo "entering... $target"
+	ssh $target "cd TFWC/exercise/comp_05.1/add-on/ && make"
+	ssh $target "cd TFWC/exercise/comp_05.2/add-on/ && make"
+	echo "leaving... $target"
+	echo ""
+done
+
+# NSF mounted systems
+for target in turkey
+do
+	echo "entering... $target"
+	ssh $target "cd /mnt/disk/soohyunc/TFWC/exercise/comp_05.1/add-on/ && make"
+	ssh $target "cd /mnt/disk/soohyunc/TFWC/exercise/comp_05.2/add-on/ && make"
+	echo "leaving... $target"
+	echo ""
+done
+
+for target in cockerel
+do
+	echo "entering... $target"
+	ssh $target "cd /mnt/disk1/soohyunc/TFWC/exercise/comp_05.1/add-on/ && make"
+	ssh $target "cd /mnt/disk1/soohyunc/TFWC/exercise/comp_05.2/add-on/ && make"
+	echo "leaving... $target"
+	echo ""
+done
+
