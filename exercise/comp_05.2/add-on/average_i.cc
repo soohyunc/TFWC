eec9893d12d6c18c417282ca2614ccf9aa724738
soohyunc <soohyunc@e14362a3-d196-dc11-b158-00301b3f055e>
2009-03-03 04:06:19 +0000

    Average for i-th flow.

---
+++ exercise/comp_05.2/add-on/average_i.cc
@@ -0,0 +1,43 @@
+// $Id$
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+
+	string option = argv[1];
+	int index = atoi(argv[2]);
+    ifstream fin (argv[3]);
+    ofstream fout_thru, fout_avg;
+    int k = 0;      // index number
+	double avg, item, time, total=0;
+
+    if(fin.is_open()) {
+		stringstream ss_thru;
+		ss_thru << "trace/" << option << "_thru_" << index << ".dat";
+		fout_thru.open(ss_thru.str().c_str());
+		while (!fin.eof()) {
+	        fin >> time >> item;
+        	total += item;
+    	    k++;
+			fout_thru << item << endl;
+		}
+		fout_thru.close();
+		fin.close();
+    }
+
+    avg = total/(k-1);
+
+	stringstream ss_avg;
+	ss_avg << "trace/" << option << "_thru_avg_" << index << ".dat";
+	fout_avg.open(ss_avg.str().c_str());
+	fout_avg << avg << endl;
+	fout_avg.close();
+
+	return 0;
+}
