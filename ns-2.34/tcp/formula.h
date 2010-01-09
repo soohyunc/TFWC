8aa11f4e74d4ab7874d8d899342a56a35641a360
soohyunc <soohyunc@e14362a3-d196-dc11-b158-00301b3f055e>
2010-01-09 18:25:10 +0000

    added printf statement at tcp/formula.h to print out TFRC's 	Tx rate(both bits/sec and pkts/sec), 	RTT, timeout, average loss rate

--- ns-2.34/tcp/formula.h
+++ ns-2.34/tcp/formula.h
@@ -12,11 +12,14 @@ static double p_to_b(double p, double rtt, double tzero, int psize, int bval)
 	if (p < 0 || rtt < 0) {
 		return MAXRATE ; 
 	}
-	res=rtt*sqrt(2*bval*p/3);
-	tmp1=3*sqrt(3*bval*p/8);
+	res = rtt * sqrt(2.0*bval*p/3.0);
+	tmp1 = 3.0 * sqrt(3.0*bval*p/8.0);
+
 	if (tmp1>1.0) tmp1=1.0;
-	tmp2=tzero*p*(1+32*p*p);
-	res+=tmp1*tmp2;
+
+	tmp2 = tzero * p * (1.0+32.0 * p * p);
+	res += tmp1 * tmp2;
+
 //	if (formula_ == 1 && p > 0.25) { 
 //		// Get closer to RFC 3714, Table 1.
 //		// This is for TCP-friendliness with a TCP flow without ECN
@@ -36,6 +39,9 @@ static double p_to_b(double p, double rtt, double tzero, int psize, int bval)
 //			res=res*2.0;
 //		}
 //	}
+
+	double temp = res;	// res val (packets per second)
+
 	// At this point, 1/res gives the sending rate in pps:
 	// 1/(rtt*sqrt(2*bval*p/3) + 3*sqrt(3*bval*p/8)*tzero*p*(1+32*p*p))
 	if (res < SAMLLFLOAT) {
@@ -47,5 +53,11 @@ static double p_to_b(double p, double rtt, double tzero, int psize, int bval)
 	if (res > MAXRATE) {
 		res = MAXRATE ; 
 	}
+
+	double now = Scheduler::instance().clock();
+	double Tx = 8.0 * res;  // unit of Tx is "bits" per second
+	printf (" %f tfrcTx: %.4f temp: %f rtt: %.5f tzero: %.4f p: %.4f",
+			now, Tx, temp, rtt, tzero, p);
+
 	return res;
 }
